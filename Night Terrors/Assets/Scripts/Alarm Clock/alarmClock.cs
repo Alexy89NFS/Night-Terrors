e628b91bc540d759c4b462ca6fdf43a87023de2b
Alexy89 <dedwa147@gmail.com>
2018-08-05 18:32:13 +0200

    Alarm clock
    Added in the time controler with the alarm clock, and the start and finish time can be changed, also the speed of the clock can be changed. I also imported a skybox which didn't work. :(

---
+++ Night Terrors/Assets/Scripts/Alarm Clock/alarmClock.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class alarmClock : MonoBehaviour {
+
+	public float time;
+	public float minsPerSec = 1;
+
+	public float startTime = 3;
+	public float finishTime = 6;
+
+	AudioSource audioSource;
+	public bool timeUp = false;
+
+	float finishfloatTime;
+
+	TextMeshPro text;
+
+	void Start() {
+		time = startTime * 3600;
+
+		finishfloatTime = finishTime * 3600;
+
+		audioSource = GetComponent<AudioSource> ();
+		text = GetComponentInChildren<TextMeshPro> ();
+	}
+
+	void Update() {
+		if (!timeUp) {
+			time += Time.deltaTime * ((1 / minsPerSec) * 60);
+		}
+
+		TimeSpan currentTime = TimeSpan.FromSeconds(time);
+		string[] tempTime = currentTime.ToString ().Split (":" [0]);
+		text.text = tempTime [0] + ":" + tempTime [1];
+
+		if (time >= finishfloatTime && !timeUp) {
+			if (audioSource.clip != null) {
+                audioSource.Play ();
+			}
+			Debug.Log ("Time Up");
+			timeUp = true;
+		}
+	}
+}
